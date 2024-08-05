1a22397b806bc9fc3c02a551d6a3a124ed7b215c
Tobi <119514764+agbacoder@users.noreply.github.com>
2024-08-05 01:39:46 +0100

    Update config.php

--- v1/config.php
+++ v1/config.php
@@ -1,7 +1,7 @@
 <?php
 include "autoload.php";
 $client = new NetciphaClient();
-$client->setSecret('84976584d77d00525c6df8d656a6394716823bb2560c0de84a0c111fd6c77ff1');
+$client->setSecret('');
 
 
 ?>
